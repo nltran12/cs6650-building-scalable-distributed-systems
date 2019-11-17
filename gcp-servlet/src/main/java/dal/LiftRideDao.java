c6594ec5bac5e9907a3330b998bdc281e2e92eb3
Nancy <tran.nan@husky.neu.edu>
2019-11-17 14:51:23 -0800

    also added hikari changes to dao

--- gcp-servlet/src/main/java/dal/LiftRideDao.java
+++ gcp-servlet/src/main/java/dal/LiftRideDao.java
@@ -7,7 +7,6 @@ import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.servlet.http.HttpServletRequest;
-import javax.sql.DataSource;
 import model.LiftRide;
 import servlet.SkierServlet;
 
@@ -15,13 +14,11 @@ import servlet.SkierServlet;
 public class LiftRideDao {
   private static final Logger LOGGER = Logger.getLogger(SkierServlet.class.getName());
 
-  public void createLiftRide(LiftRide newLiftRide, HttpServletRequest req) {
+  public void createLiftRide(LiftRide newLiftRide) {
     String insertQueryStatement = "INSERT INTO LiftRides (skierId, resortId, seasonId, dayId, "
         + "time, liftId, vertical) VALUES (?,?,?,?,?,?,?)";
 
-    DataSource pool = (DataSource) req.getServletContext().getAttribute("my-pool");
-
-    try (Connection conn = pool.getConnection();
+    try (Connection conn = HikariDS.getConnection();
     PreparedStatement preparedStatement = conn.prepareStatement(insertQueryStatement)) {
       preparedStatement.setInt(1, newLiftRide.getSkierId());
       preparedStatement.setInt(2, newLiftRide.getResortId());
@@ -43,8 +40,7 @@ public class LiftRideDao {
     int totalVertical = 0;
     String selectCreditCard = "SELECT vertical FROM LiftRides WHERE skierId=? AND resortId=? AND"
         + " seasonId=? AND dayId=?;";
-    DataSource pool = (DataSource) req.getServletContext().getAttribute("my-pool");
-    try (Connection conn = pool.getConnection();
+    try (Connection conn = HikariDS.getConnection();
         PreparedStatement preparedStatement = conn.prepareStatement(selectCreditCard)) {
       preparedStatement.setInt(1, skierId);
       preparedStatement.setInt(2, resortId);
