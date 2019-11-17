571a20a791ceab86fc8731e21d2673a2420ef587
Nancy <tran.nan@husky.neu.edu>
2019-11-17 13:51:39 -0800

    adding actual project

---
+++ gcp-servlet/src/main/java/model/Resorts.java
@@ -0,0 +1,13 @@
+package model;
+
+public class Resorts {
+  private ResortInfo resorts;
+
+  public Resorts(ResortInfo resorts) {
+    this.resorts = resorts;
+  }
+
+  public ResortInfo getResorts() {
+    return resorts;
+  }
+}
---
+++ gcp-servlet/src/main/java/model/SkierResorts.java
@@ -0,0 +1,13 @@
+package model;
+
+public class SkierResorts {
+  private SkierVertical resorts;
+
+  public SkierResorts(SkierVertical resorts) {
+    this.resorts = resorts;
+  }
+
+  public SkierVertical getResorts() {
+    return resorts;
+  }
+}
